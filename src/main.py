c1b09edd3cdc5d095a245ef6264a59b3d69c1e5d
Dan Nelson <44625150+danelson813@users.noreply.github.com>
2025-11-17 20:49:56 -0800

    trying to use vscode

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
-# omak.src/main.py
+# omad_template/src/main.py
 
+import requests
+# import pandas as pd
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+url = 'https://books.toscrape.com'
+
+ua = UserAgent()
+headers = {'user-agent': ua.random}
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.content, 'html.parser')
+title = soup.title.text
+print(title)
